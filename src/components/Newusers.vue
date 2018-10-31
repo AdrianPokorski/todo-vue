7cdd8c113f28562a7b21a5cbc687d63cd7e252d4
Adrian <adrian.pokorski@brandwise.pl>
2018-10-31 11:06:30 +0100

    init commit/create project

---
+++ src/components/Newusers.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <label class="mr-4" for="name">Imię</label>
+          <input class="mr-4" type="text" name="name">
+          <label class="mr-4" for="surname">Nazwisko</label>
+          <input class="mr-4" type="text" name="surname">
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Newusers',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
